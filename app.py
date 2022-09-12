7366cf3128573117d510723519a8365802b87e6e
expliyh <expliyh@outlook.com>
2022-09-12 10:25:33 +0800

    加入图片获取

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import pixiv_api
 app = Flask(__name__)
 
 
-@app.route('/image/<int:img_id>')
+@app.route('/json/<int:img_id>')
 def get_detail(img_id):
     pixiv = pixiv_api.Pixiv()
     return pixiv.get_image_json(img_id)
+
+
+@app.route('/image/<int:img_id>')
+def get_detail(img_id):
+    pixiv = pixiv_api.Pixiv()
+    return pixiv.get_image(img_id)
 
 
 @app.route('/')
--- pixiv_api/__init__.py
+++ pixiv_api/__init__.py
@@ -8,17 +8,24 @@ import json
 
 class Pixiv:
 
-    def get_image(self,img_id:int):
-        json_str=self.get_image_json(img_id)
+    def get_image(self, img_id: int):
+        json_str = self.get_image_json(img_id)
+        json_obj = json.loads(json_str)
+        headers = {'Referer': 'https://www.pixiv.net'}
+        url = json_obj['illust']['meta_single_page']['original_image_url']
+        req = requests.get(url=url, headers=headers, verify=False)
+        return req.content
+
 
     def get_image_json(self, img_id: int):
         sql = "SELECT * FROM artworks WHERE id = %s" % img_id
         cursor = self.cache.cursor()
-        row = cursor.fetchone(sql)
+        cursor.execute(sql)
+        row = cursor.fetchone()
         if row != 0:
             time_now = time.time()
             time_cache = int(row[2])
-            if time_now - time_cache<=86400:
+            if time_now - time_cache <= 86400:
                 return row[1]
         else:
             json_str = self.api.illust_detail(img_id)
@@ -29,8 +36,8 @@ class Pixiv:
             cursor.close()
             return json_str
         json_str = self.api.illust_detail(img_id)
-        print("Cache %s updated!"%img_id)
-        sql="UPDATE artworks SET json = '%s', time = %s WHERE id = %s"%(json_str, time.time(), img_id)
+        print("Cache %s updated!" % img_id)
+        sql = "UPDATE artworks SET json = '%s', time = %s WHERE id = %s" % (json_str, time.time(), img_id)
         return json_str
 
     def __init__(self):
