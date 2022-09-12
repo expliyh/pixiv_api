4402b1766b84406c30a1c16f3b6a0384fe9549ce
expliyh <expliyh@outlook.com>
2022-09-12 10:34:58 +0800

    加入图片获取

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import pixiv_api
 app = Flask(__name__)
 
 
-@app.route('/json/<int:imgid>')
-def get_detail(imgid):
+@app.route('/json/<int:img_id>')
+def get_detail(img_id):
     pixiv = pixiv_api.Pixiv()
-    return pixiv.get_image_json(imgid)
+    return pixiv.get_image_json(img_id)
 
 
 @app.route('/image/<int:img_id>')
